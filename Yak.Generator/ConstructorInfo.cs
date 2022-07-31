acfe5cd2d9319264a702da2a9ea9cbbd4a2e92d0
Disketteman <109335133+disketteman@users.noreply.github.com>
2022-07-31 06:55:21 +0200

    Namespace changed to file scope.

--- Yak.Generator/ConstructorInfo.cs
+++ Yak.Generator/ConstructorInfo.cs
@@ -1,30 +1,29 @@
-﻿namespace Yak.Generator
+﻿namespace Yak.Generator;
+
+internal sealed class ConstructorInfo
 {
-    internal sealed class ConstructorInfo
-    {
-        public string? TypeName { get; }
-        public ImmutableArray<string> ParameterTypes { get; }
+    public string? TypeName { get; }
+    public ImmutableArray<string> ParameterTypes { get; }
         
-        public ConstructorInfo(string? typeName, ImmutableArray<string> parameterTypes)
-        {
-            TypeName = typeName;
-            ParameterTypes = parameterTypes;
-        }
+    public ConstructorInfo(string? typeName, ImmutableArray<string> parameterTypes)
+    {
+        TypeName = typeName;
+        ParameterTypes = parameterTypes;
+    }
 
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(this, obj)) return true;
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj)) return true;
 
-            return obj is ConstructorInfo other && TypeName == other.TypeName &&
-                   ParameterTypes.SequenceEqual(other.ParameterTypes);
-        }
+        return obj is ConstructorInfo other && TypeName == other.TypeName &&
+               ParameterTypes.SequenceEqual(other.ParameterTypes);
+    }
 
-        public override int GetHashCode()
+    public override int GetHashCode()
+    {
+        unchecked
         {
-            unchecked
-            {
-                return ((TypeName != null ? TypeName.GetHashCode() : 0) * 397) ^ ParameterTypes.GetHashCode();
-            }
+            return ((TypeName != null ? TypeName.GetHashCode() : 0) * 397) ^ ParameterTypes.GetHashCode();
         }
     }
 }
